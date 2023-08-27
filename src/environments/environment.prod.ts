8ff12fbfe80723f0eed3c8f4129b9aaf77cf5948
Ahmed Essam <70452432+A7med-Essam@users.noreply.github.com>
2023-08-27 21:48:40 +0300

    fix issues

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,9 @@ export const environment = {
   production: true,
   BaseUrl: 'https://manager.lowcalories.ae/',
   BaseUrl2: 'http://thelowcalories.com:51/api/',
-  BaseUrl3: 'https://lowcalories.ae/api/v4/',
-  BaseUrl4: 'https://lowcalories.ae/api/v3/'
+  // BaseUrl3: 'https://lowcalories.ae/api/v4/',
+  // BaseUrl4: 'https://lowcalories.ae/api/v3/'
+  BaseUrl3: 'https://api.lowcalories.ae/api/v4/',
+  BaseUrl4: 'https://api.lowcalories.ae/api/v3/'
 };
+// http://api.lowcalories.ae/
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,8 +6,10 @@ export const environment = {
   production: false,
   BaseUrl: 'https://manager.lowcalories.ae/',
   BaseUrl2: 'http://thelowcalories.com:51/api/',
-  BaseUrl3: 'https://lowcalories.ae/api/v4/',
-  BaseUrl4: 'https://lowcalories.ae/api/v3/'
+  // BaseUrl3: 'https://lowcalories.ae/api/v4/',
+  // BaseUrl4: 'https://lowcalories.ae/api/v3/'
+  BaseUrl3: 'https://api.lowcalories.ae/api/v4/',
+  BaseUrl4: 'https://api.lowcalories.ae/api/v3/'
 };
 
 /*
