454106269930cb7957cb34ff773c88d9b58dc8e8
Ahmed Essam <70452432+A7med-Essam@users.noreply.github.com>
2023-10-25 19:52:19 +0300

    fix base

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   BaseUrl: 'https://manager.lowcalories.ae/',
-  BaseUrl2: 'http://thelowcalories.com:51/api/',
+  BaseUrl2: 'https://thelowcalories.com:52/api/',
   BaseUrl3: 'https://api.lowcalories.ae/api/v4/',
   BaseUrl4: 'https://api.lowcalories.ae/api/v3/'
 };
